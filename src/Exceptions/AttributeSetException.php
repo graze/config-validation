a663a7912c56add2d1372bc08ae3184755c6e399
Harry Bragg <harry.bragg@graze.com>
2017-10-02 18:05:18 +0100

    add copywrite info

--- src/Exceptions/AttributeSetException.php
+++ src/Exceptions/AttributeSetException.php
@@ -1,7 +1,14 @@
 <?php
-
-/*
- * This file is based on KeySetException of Respect/Validation.
+/**
+ * This file is part of graze/config-validation.
+ *
+ * Copyright (c) 2017 Nature Delivered Ltd. <https://www.graze.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * @license https://github.com/graze/config-validation/blob/master/LICENSE.md
+ * @link    https://github.com/graze/config-validation
  */
 
 namespace Graze\ConfigValidation\Exceptions;
